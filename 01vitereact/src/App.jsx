a5499b75045cfbb306ac30831a9bace82f844d3e
Harshit nehra <nehraharshit01@gmail.com>
2024-09-13 18:01:43 +0530

    first react file

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Chai from './Chai'
+function App() {
+const username = "chai aur harshit"
+  return (
+    <>
+    < Chai/>
+    <h1>chai aur react</h1>
+    <p>test para   {username}</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>harshit ki chai ready</h3>
+    )
+}
+
+export default Chai
