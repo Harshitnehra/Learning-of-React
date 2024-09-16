5bf1c7d856c93cfc6e3c4b9e611fc74cb4fa5307
Harshit nehra <nehraharshit01@gmail.com>
2024-09-16 18:21:20 +0530

    add bootstrap

---
+++ 04Compouents/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import CompAppName from "./CompAppName";
+import Todoitem from "./TodoItem"
+function App() {
+  return (
+    <center>
+      <h1>todolist</h1>
+      <div class="container text-center">
+        <CompAppName></CompAppName>
+        <Todoitem></Todoitem>
+        <div class="row">
+          <div class="col-6">siddharth nehra</div>
+          <div class="col-4">24/7/2005</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              danger
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
---
+++ 04Compouents/src/TodoItem.jsx
@@ -0,0 +1,13 @@
+function Todoitem(){
+    return(
+    <div class="row">
+          <div class="col-6">harshit nehra</div>
+          <div class="col-4">20/7/2003</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              danger
+            </button>
+          </div>
+        </div>);
+}
+export default Todoitem
