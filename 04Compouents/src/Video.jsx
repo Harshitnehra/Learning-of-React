fe948ba11e26993db0ee44ac94172b10035f3a3d
Harshit nehra <nehraharshit01@gmail.com>
2024-09-23 11:59:25 +0530

    compounets

---
+++ 04Compouents/src/Video.jsx
@@ -0,0 +1,13 @@
+import './Style.css'
+import { useState } from 'react'
+function Video({tital}) {
+    const [count, setCount] = useState(0)
+    let a ="fdgh"
+    let bg = 'dark'
+    return(
+        <>
+        <h1 className={bg}>{tital}jbbdjv</h1>
+        </>
+    )
+}
+export default Video
