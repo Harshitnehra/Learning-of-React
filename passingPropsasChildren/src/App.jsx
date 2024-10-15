3e2dfe3ab4f430753c4489ec28aff683737f9479
Harshit nehra <nehraharshit01@gmail.com>
2024-10-15 23:57:06 +0530

    props

--- passingPropsasChildren/src/App.jsx
+++ passingPropsasChildren/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import Card from "./Components/Card"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p>harshit</p>
-    </>
+    <div>
+      <p>harshoit nehra</p>
+      <Card name="love">
+        <h1>siddhu handsome hai</h1>
+        <p>harshit is good boy</p>
+      </Card>
+    </div>
   )
 }
 
---
+++ passingPropsasChildren/src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const card = ({name}) => {
+  return (
+    <div>{name}</div>
+  )
+}
+
+export default card
