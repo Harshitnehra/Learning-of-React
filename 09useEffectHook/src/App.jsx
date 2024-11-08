d78b27b183ce05a238a7768c20f9a0e8743e759e
Harshit nehra <nehraharshit01@gmail.com>
2024-11-08 14:59:14 +0530

    react pro

--- 09useEffectHook/src/App.jsx
+++ 09useEffectHook/src/App.jsx
@@ -1,16 +1,15 @@
+import "./App.css";
+import Navigation from './components/Navigation'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-        
-    </>
-  )
+  <div>
+    <Navigation />
+    {/* <HeroSection /> */}
+  </div>
+      )
 }
 
 export default App
