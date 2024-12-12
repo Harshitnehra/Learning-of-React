816168bbc9e9be9bea7d99e6f905f13f230f6056
Harshit nehra <nehraharshit01@gmail.com>
2024-12-12 15:15:17 +0530

    react router

--- 17reactRouter/src/components/About.jsx
+++ 17reactRouter/src/components/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { useNavigate } from "react-router-dom"
 const About = () => {
+  const navigate = useNavigate();
+  function handleclick(){
+    navigate("/Dependency")
+  }
   return (
     <div>
       <h1>about</h1>
+      <button onClick={handleclick}>
+        click to go deshbord
+      </button>
     </div>
   )
 }
